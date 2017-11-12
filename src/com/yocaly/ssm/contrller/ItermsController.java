93c385fd848f05bf28c215efa557ab0c080b4915
王志刚 <wangzhigang@yocaly.com>
2017-11-13 00:28:07 +0800

    second commit

---
+++ src/com/yocaly/ssm/contrller/ItermsController.java
@@ -0,0 +1,31 @@
+package com.yocaly.ssm.contrller;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+import com.yocaly.ssm.model.Items;
+import com.yocaly.ssm.model.ItemsCustomer;
+import com.yocaly.ssm.service.ItemsService;
+
+@Controller
+public class ItermsController {
+	@Autowired
+	private ItemsService itemsService;
+
+	// 商品的查询
+	@RequestMapping("/queryItems.action")
+	public ModelAndView queryIems() throws Exception {
+		// TODO Auto-generated method stub
+		List<ItemsCustomer> itemsList = itemsService.findItemsList(null);
+		
+		ModelAndView mav = new ModelAndView();
+		mav.addObject("itemsList", itemsList);
+		mav.setViewName("items/itemsList");
+		return mav;
+	}
+}
